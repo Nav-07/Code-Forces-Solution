41e2f48652b0e712866b5fd143c30a78186a1279
= <navneil2006@gmail.com>
2020-04-28 08:21:22 +1000

    Solution added for 266-A

---
+++ 266-A.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int n = 0; cin >> n;
+	string s; cin >> s;
+	int ans = 0;
+	if (n > 1) {
+		for (int i = 0; i < n-1; i++) {
+			if (s[i] == s[i+1])
+				ans++;
+		}
+	}
+	cout << ans << endl;
+}
