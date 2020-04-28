061e140ec6191a54516f82ed2117405947169680
= <navneil2006@gmail.com>
2020-04-28 21:53:49 +1000

    Added Solution for 133-A

---
+++ 133-A.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main() {
+	string s;
+	cin >> s;
+	string ans = "NO";
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] == 'H' || s[i] == 'Q' || s[i] == '9') {
+			ans = "YES";
+			break;
+		}
+	}
+	cout << ans << endl;
+	return 0;
+}
