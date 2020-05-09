c5b993e9bb50f3e20842e31ede381c5c2cf03b5d
= <navneil2006@gmail.com>
2020-05-09 20:14:15 +1000

    Added Solution for 112-A

---
+++ 112-A.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string a, b; cin >> a >> b;
+	string num1, num2;
+	for (int i = 0; i < a.size(); i++) {
+		if (a[i] >= 65 && a[i] <= 90) {
+			a[i] += 32;
+		} 
+		if (b[i] >= 65 && b[i] <= 90) {
+			b[i] += 32;
+		} 
+	}
+	if (a < b)
+		cout << "-1" << endl;
+	else if (a > b) 
+		cout << "1" << endl;
+	else if (a == b)
+		cout << "0" << endl;
+	return 0;
+}
