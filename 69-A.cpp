5f6af7028febd058cb92c831a4ff04180702e4f8
= <navneil2006@gmail.com>
2020-05-13 23:05:11 +1000

    added solution for 69-A

---
+++ 69-A.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n=0, x=0, y=0, z=0;
+	cin >> n;
+	while (n--) {
+		int a,b,c;cin>>a>>b>>c;
+		x+=a;y+=b;z+=c;
+	}
+	if (x==0&&y==0&&z==0)
+		cout << "YES" << endl;
+	else
+		cout << "NO" << endl;
+	return 0;
+}
