99f9fd12b82a311ed7f26b17a62e5a21057cb5c1
= <navneil2006@gmail.com>
2020-05-23 22:45:14 +1000

    added solution for 1351A

---
+++ 1351-A.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <utility>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+#define push_back pb
+typedef long long int lli;
+
+void solve() {
+	int t; cin >> t;
+	while (t--) {
+		lli a, b;
+		cin >> a >> b;
+		cout << a+b << endl;
+	}
+}
+int main() {
+	solve();
+	return 0;
+}
---
+++ code-templates/template1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <utility>
+#include <cmath>
+using namespace std;
+
+#define push_back in
+typedef long long int lli;
+
+void solve() {
+	
+}
+int main() {
+	solve();
+	return 0;
+}
